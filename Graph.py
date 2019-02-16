9d0be56ad61ed5394331fbec8888b9ff9198bb80
saikaveti <saileshkaveti@hotmail.com>
2019-02-15 20:05:26 -0500

    can quickly build graph

--- Graph.py
+++ Graph.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 class Vertex:
 
@@ -32,6 +33,35 @@ class Vertex:
         else:
             return -1
 
+    def get_nearby_vertices(self, s):
+
+        list = []
+
+        theta1_set = set([])
+        theta2_set = set([])
+        theta3_set = set([])
+
+        theta1_set.add(max(0, self.theta1 - s))
+        theta1_set.add(self.theta1)
+        theta1_set.add(min(180, self.theta1 + s))
+
+        theta2_set.add(max(0, self.theta2 - s))
+        theta2_set.add(self.theta2)
+        theta2_set.add(min(180, self.theta2 + s))
+
+        theta3_set.add(max(0, self.theta3 - s))
+        theta3_set.add(self.theta3)
+        theta3_set.add(min(180, self.theta3 + s))
+
+        for t1 in theta1_set:
+            for t2 in theta2_set:
+                for t3 in theta3_set:
+                    line = "nodeID" + "+" + str(t1) + "+" + str(t2) + "+" + str(t3)
+                    list.append(line)
+
+        return list
+
+
 
 class Graph:
 
@@ -51,7 +81,9 @@ class Graph:
             self.graph_map[v.nodeID] = []
             self.id_map[v.nodeID] = v
 
-    def create_edges(self):
+        print("finished initialize")
+
+    def create_edges_raw(self):
 
         count  = 0
 
@@ -71,14 +103,42 @@ class Graph:
         csv_file_object.write("v1_nodeID,v2_nodeID,distance")
         csv_file_object.write("\n")
 
+        count = 0
+
         for key in self.graph_map:
             v1 = self.id_map[key]
-            '''
-            for v2 in self.vertices:
-                if v1.within_threshold(v2, 5) != -1:
-                    csv_str = "{},{},{!s}".format(v1.nodeID, v2.nodeID, round(v1.within_threshold(v2, 5), 3))
+
+            nearby_vertices = v1.get_nearby_vertices(5)
+
+            for v2_nodeID in nearby_vertices:
+                if v1.nodeID != v2_nodeID:
+                    csv_str = "{},{},{!s}".format(v1.nodeID, v2_nodeID, round(v1.within_threshold(self.id_map[v2_nodeID], 5), 3))
                     csv_file_object.write(csv_str)
                     csv_file_object.write("\n")
-            '''
 
-            
+            count = count + 1
+
+            if count % 1000 == 0:
+                print("write count: " + str(count))
+
+        print("finished writing edges to csv")
+
+    def build_graph(self, edges_csv):
+        edgesDF = pd.read_csv("edges.csv")
+
+        for index, row in edgesDF.iterrows():
+
+            v1_nodeID = row['v1_nodeID']
+            v2_nodeID = row['v2_nodeID']
+
+            v1 = self.id_map[v1_nodeID]
+            v2 = self.id_map[v2_nodeID]
+
+            tempList = self.graph_map[v1.nodeID]
+            tempList.append(v2)
+            self.graph_map[v1.nodeID] = tempList
+
+            if index % 1000 == 0:
+                print("build count: " + str(index))
+
+        print("finished building graph")
