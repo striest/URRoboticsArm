153605bcc29cb1fb716ec669f9142a5709a1f5e4
striest <striest@Samuels-MacBook-Pro.local>
2019-03-01 20:28:47 -0500

    vertex tree has the whole control pipeline

--- ControllerXYZ.py
+++ ControllerXYZ.py
@@ -6,7 +6,7 @@ def getXYZ(prev_coord, prev_gradient):
     Returns an integer x,y,z vector from the current controller inputs.
     Allows for continuous controller inputs, or simulates that at least.
     """
-    speed = 0.1             # Controls the speed the controller outputs change.
+    speed = 0.01             # Controls the speed the controller outputs change.
     null_zone = 0           # Controls the null margin for the controller.
 
     events = inputs.get_gamepad()
@@ -20,7 +20,6 @@ def getXYZ(prev_coord, prev_gradient):
 
         if (type == "Absolute"):
             state = (state - 127.5) / 127.5 * speed
-            state = int(round(state, 1) * 10)
 
             if (abs(state) < null_zone):
                 state = 0
@@ -32,10 +31,9 @@ def getXYZ(prev_coord, prev_gradient):
             elif (code == "ABS_RZ"):
                 gradient[2] = -state
 
-    new_coord = (0,0,0)
+    new_coord = [0,0,0]
     for i in range(3):
         new_coord[i] = prev_coord[i] + gradient[i]
-
     return new_coord, gradient
 
 def main():
--- vertex_tree.py
+++ vertex_tree.py
@@ -1,4 +1,5 @@
 import math
+import time
 import pandas as pd
 from Graph import *
 from ControllerXYZ import *
@@ -133,8 +134,9 @@ def main():
 	graph.initialize(verticesDF)
 	graph.build_graph()
 	
+
 	root = load_from_csv('vertices.csv')
-	print('\n')
+	print('finished tree')
 	# root.preorder()
 	# print(root.countnodes())
 	# print(root.children)
@@ -161,18 +163,24 @@ def main():
 	for p in path:
 		print(p)
 
-	x, x_prev, gx_prev = 0
-	y, y_prev, gy_prev = 0
-	z, z_prev, gz_prev = 0
+	x, x_prev, gx_prev = 0, 0, 0 
+	y, y_prev, gy_prev = 0, 0, 0
+	z, z_prev, gz_prev = 0, 0, 0
 	p_node = tree_lookup(root, x, y, z, k=10)
 	p_thetas = p_node.data
-	p_nodestr = 'nodeID+{}+{}+{}'.format(p_thetas[0], p_thetas2[1], p_thetas[2])
+	p_nodestr = 'nodeID+{}+{}+{}'.format(p_thetas[0], p_thetas[1], p_thetas[2])
 	while True:
-		(x, y, z), (gx, gy, gz) = getXYZ((x, y, z), (gx_prev, gy_prev, gz_prev))
+		pos, grad = getXYZ(list((x, y, z)), list((gx_prev, gy_prev, gz_prev)))
+		x = pos[0]
+		y = pos[1]
+		z = pos[2]
+		gx = grad[0]
+		gy = grad[1]
+		gz = grad[2]
 		print('x:{}, y:{}, z{}'.format(x, y, z))
 
 		c_node = tree_lookup(root, x, y, z, k=10)
-		thetas = c_node.data
+		c_thetas = c_node.data
 		c_nodestr = 'nodeID+{}+{}+{}'.format(c_thetas[0], c_thetas[1], c_thetas[2])
 
 		path = graph.dfs(graph.id_map[p_nodestr], graph.id_map[c_nodestr])
@@ -182,11 +190,12 @@ def main():
 		print('Node2=', nodestr2)
 
 		for p in path:
-		print(p)
+			print(p)
 
 		(x_prev, y_prev, z_prev) = (x, y, z)
 		(gx_prev, gy_prev, gz_prev) = (gx, gy, gz)
 		p_node = c_node
+		p_nodestr = c_nodestr
 
 if __name__ == '__main__':
 	main()
